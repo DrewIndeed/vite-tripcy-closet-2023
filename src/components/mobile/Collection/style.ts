fd2731ce699fb526444e1c5c77e3ca483a0f1a09
an.le6 <anle.drewindeed@gmail.com>
2023-05-21 14:59:00 +0700

    77. test: text layout

--- src/components/mobile/Collection/style.ts
+++ src/components/mobile/Collection/style.ts
@@ -156,7 +156,7 @@ export const MobileCollectionWrapper = styled.section<MobileCollectionWrapperTyp
 
   .see-more {
     margin-top: auto;
-    margin-bottom: 4rem;
+    margin-bottom: 8rem;
     z-index: 40;
 
     .arrow-button {
