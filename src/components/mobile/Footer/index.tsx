1ce8e49f6b61753d071f314b17e85e2eb64916f7
an.le6 <anle.drewindeed@gmail.com>
2023-07-06 14:47:30 +0700

    142. test: sections height

--- src/components/mobile/Footer/index.tsx
+++ src/components/mobile/Footer/index.tsx
@@ -116,12 +116,13 @@ const FOOTER_SECTIONS = [
 
 const Footer = () => {
   const policiesViewControls = useAnimation();
-  const scrollToBot = useCallback(
-    () => window?.scrollTo({ top: 1000, behavior: "smooth" }),
-    []
-  );
   const [viewerContent, setViewerContent] = useState<Record<string, any>>({});
   const isOpen = Object.keys(viewerContent).length > 0;
+
+  useEffect(() => {
+    const scrollingElement = document.scrollingElement || document.body;
+    scrollingElement.scrollTop = scrollingElement.scrollHeight;
+  }, []);
 
   const onMenuOpen = useCallback(() => {
     if (!isOpen) {
@@ -210,7 +211,6 @@ const Footer = () => {
         {FOOTER_SECTIONS.map((section: Record<string, any>, idx: number) => {
           return (
             <motion.div
-              onAnimationStart={() => scrollToBot()}
               initial={{ opacity: 0 }}
               whileInView={{ opacity: 1 }}
               transition={{ duration: 0.2 + (0.05 * idx + 1) }}
